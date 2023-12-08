50f97824a35ca212c4e33f86d107403eb4882421
minaiscoding <khadir04kh@gmail.com>
2023-12-08 19:07:43 +0100

    added dummy data to database

--- server/backend/backendapp/models.py
+++ server/backend/backendapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class Utilisateurs(models.Model):
+    NomUtilisateur = models.CharField(max_length=255, unique=True)
+    Email = models.EmailField(unique=True)
+    MotDePasse = models.CharField(max_length=255)
+    Role = models.CharField(max_length=50)  # "admin," "moderator," "user"
+
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        self.MotDePasse = make_password(self.MotDePasse)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.NomUtilisateur
+
+class Articles(models.Model):
+    Titre = models.CharField(max_length=255)
+    Resume = models.TextField()
+    auteurs = models.CharField(max_length=255)
+    Institution = models.CharField(max_length=255)
+    date = models.DateField()
+    MotsCles = models.CharField(max_length=255)
+    text = models.TextField()
+    URL_Pdf = models.CharField(max_length=255)
+    RefBib = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Titre
+
+class Favoris(models.Model):
+    UtilisateurID = models.ForeignKey(Utilisateurs, on_delete=models.CASCADE)
+    ArticleID = models.ForeignKey(Articles, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('UtilisateurID', 'ArticleID')
+
+    def __str__(self):
+        return f'{self.UtilisateurID.NomUtilisateur} - {self.ArticleID.Titre}'

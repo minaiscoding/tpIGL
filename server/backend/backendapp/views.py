bf57397e3ff185c52d655cb7aa0bbbb61fb84f5e
Wifak-Herkat <lw_herkat@esi.dz>
2023-12-25 14:22:56 +0100

    first commit

--- server/backend/backendapp/views.py
+++ server/backend/backendapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password
 from rest_framework import generics
 from .models import Utilisateurs, Articles, Favoris
 from .serializers import UtilisateursSerializer, ArticlesSerializer, FavorisSerializer
@@ -5,6 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from elasticsearch_dsl import Search
 from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+
 
 
 class UtilisateursListView(generics.ListAPIView):
@@ -40,3 +44,44 @@ class SearchView(APIView):
 
         # Return the serialized results as JSON
         return Response(serializer.data)
+    
+    
+
+
+class LogoutView(APIView):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
+    
+'''class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('NomUtilisateur')
+        password = request.data.get('MotDePasse')
+
+        user = authenticate(request, NomUtilisateur=username, MotDePasse=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)'''
+            
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Fetch user by username
+        try:
+            user = Utilisateurs.objects.get(NomUtilisateur=username)
+        except Utilisateurs.DoesNotExist:
+            user = None
+
+        if user is not None and check_password(password, user.MotDePasse):
+            # If user is authenticated, log them in
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            # If authentication fails, return an error response
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)

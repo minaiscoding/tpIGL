50542a0c1a307f387d35ae0e1d026b8c4b2acd73
Wifak-Herkat <lw_herkat@esi.dz>
2023-12-28 10:01:26 +0100

    affichage dummy data dans DetailsArticle

--- client/frontend/src/components/InfoArticle.js
+++ client/frontend/src/components/InfoArticle.js
@@ -1,77 +1,209 @@
-import React from 'react';
-
-
+import React from "react";
 
 // ADD AN ONCHANGE FUNCTION FOR THE EDIT FORM
 // ADD HANDLE SUBLIT FOR THE BUTTON SAVE (INTEGRAION FCTN)
 // ADD CANCEL INTEGRATION FNCTION
 
-
 const info = {
   titre: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   auteurs: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  institutions: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  resume: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  motsCles: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  texte: "Some random text for the article content. Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-  biblio: "More random text for the bibliography. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  institutions:
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  resume:
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  motsCles:
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  texte:
+    "Some random text for the article content. Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  biblio:
+    "More random text for the bibliography. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 };
 
-const style1 = 'font-Futura text-left text-xl text-purple2 font-semibold';
-const style2 = 'font-Futura text-navBg text-base text-wrap ml-2 mb-2';
-const style3 ='font-Futura text-navBg text-base text-wrap ml-2 mb-2 border-solid border-[1px] border-navBg px-2  rounded-md w-[90%] w-full ';
+const style1 = "font-Futura text-left text-xl text-purple2 font-semibold";
+const style2 = "font-Futura text-navBg text-base text-wrap ml-2 mb-2";
+const style3 =
+  "font-Futura text-navBg text-base text-wrap ml-2 mb-2 border-solid border-[1px] border-navBg px-2  rounded-md w-[90%] w-full ";
 
 function InfoArticle(props) {
+  const [formData, setFormData] = useState(props.article || {});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (props.edit) {
+      // Update form data when 'edit' prop changes
+      setFormData(props.article || {});
+    } else {
+      // Fetch articles when component mounts
+      fetchArticles();
+    }
+  }, [props.edit, props.article]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/articles");
+      const fetchedArticles = response.data;
+      setFormData(fetchedArticles[1] || {}); // Assuming you want to display the first article
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8000/api/articles/${formData.id}`);
+      // Assuming that the server returns a success status (2xx)
+      // You may want to handle different response status codes appropriately
+      console.log("Article deleted successfully");
+      // Add any additional logic after deletion if needed
+    } catch (error) {
+      console.error("Error deleting article:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   return (
-    <div className={`bg-[#ffff] mx-4 border-solid rounded-sm px-8 py-2 max-h-[75%] border-navBg  mt-20 sm:mt-8  mb-4 w-[85%]`}>
-      {!props.edit ? (
-        <div className='flex flex-col items-start justify-center text-left'>
-          <h2 className={style1}> Titre de l'article :</h2>
-          <p className={style2}>{info.titre}</p>
+    <div
+      className={`bg-[#ffff] mx-4 border-solid rounded-sm px-8 py-2 max-h-[75%] border-navBg mt-20 sm:mt-8 mb-4 w-[85%]`}
+    >
+      {props.edit ? (
+        <div className="flex flex-col items-start text-left gap-2">
+          <label htmlFor="titre" className={style1}>
+            {" "}
+            Titre de l'article :
+          </label>
+          <input
+            type="text"
+            id="titre"
+            name="titre"
+            value={formData.Titre || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={style1}>Les auteurs :</h2>
-          <p className={style2}>{info.auteurs}</p>
+          <label htmlFor="auteurs" className={style1}>
+            Les auteurs :
+          </label>
+          <textarea
+            type="text"
+            id="auteurs"
+            name="auteurs"
+            value={formData.auteurs || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={style1}> Institusions :</h2>
-          <p className={style2}>{info.institutions}</p>
+          <label htmlFor="institutions" className={style1}>
+            {" "}
+            Institusions :
+          </label>
+          <textarea
+            type="text"
+            id="institutions"
+            name="institutions"
+            value={formData.Institution || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={style1}> Résumé :</h2>
-          <p className={style2}>{info.resume}</p>
+          <label htmlFor="resume" className={style1}>
+            {" "}
+            Résumé :
+          </label>
+          <textarea
+            type="text"
+            id="resume"
+            name="resume"
+            value={formData.Resume || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={`${style1} ml-2`}> Mots clés : </h2>
-          <p className={style2}>{info.motsCles}</p>
+          <label htmlFor="motsCles" className={style1}>
+            {" "}
+            Mots clés :
+          </label>
+          <textarea
+            type="text"
+            id="motsCles"
+            name="motsCles"
+            value={formData.MotsCles || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={style1}> Texte :</h2>
-          <p className={`${style2} ml-2`}>{info.texte}</p>
+          <label htmlFor="texte" className={style1}>
+            {" "}
+            Texte :
+          </label>
+          <textarea
+            type="text"
+            id="texte"
+            name="texte"
+            value={formData.text || ""}
+            onChange={handleChange}
+            className={style3}
+          />
 
-          <h2 className={style1}> Bibiliographie : </h2>
-          <p className={`${style2} ml-2`}>{info.biblio}</p>
+          <label htmlFor="biblio" className={style1}>
+            {" "}
+            Bibiliographie :
+          </label>
+          <textarea
+            type="text"
+            id="biblio"
+            name="biblio"
+            value={formData.RefBib || ""}
+            onChange={handleChange}
+            className={style3}
+          />
+
+          <button
+            className="bg-[#6B30E4] text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2"
+            onClick={() => {
+              console.log("Save button clicked");
+              console.log(formData);
+            }}
+          >
+            Enregistrer
+          </button>
+          <button
+            className="bg-yellow text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2"
+            onClick={() => {
+              console.log("Cancel button clicked");
+              // props.setEdit(false);
+            }}
+          >
+            Annuler
+          </button>
         </div>
       ) : (
-        <div className='flex flex-col items-start text-left gap-2  '>
-         
-            <label htmlFor='titre' className={style1}> Titre de l'article :</label>
-            <input type='text' id='titre' name='titre'  rows={5} defaultValue={info.titre} className={style3} />
+        <div className="flex flex-col items-start justify-center text-left">
+          <h2 className={style1}> Titre de l'article :</h2>
+          <p className={style2}>{formData.Titre || ""}</p>
 
-            <label htmlFor='auteurs' className={style1}>Les auteurs :</label>
-            <textarea type='text' id='auteurs' name='auteurs'  rows={5} defaultValue={info.auteurs} className={style3} />
+          <h2 className={style1}>Les auteurs :</h2>
+          <p className={style2}>{formData.auteurs || ""}</p>
 
-            <label htmlFor='institutions' className={style1}> Institusions :</label>
-            <textarea type='text' id='institutions' name='institutions'  rows={5} defaultValue={info.institutions} className={style3} />
+          <h2 className={style1}> Institusions :</h2>
+          <p className={style2}>{formData.Institution || ""}</p>
 
-            <label htmlFor='resume' className={style1}> Résumé :</label>
-            <textarea type='text' id='resume' name='resume' defaultValue={info.resume} className={style3} />
+          <h2 className={style1}> Résumé :</h2>
+          <p className={style2}>{formData.Resume || ""}</p>
 
-            <label htmlFor='motsCles' className={style1}> Mots clés : </label>
-            <textarea type='text' id='motsCles' name='motsCles' defaultValue={info.motsCles} className={style3} />
+          <h2 className={`${style1} ml-2`}> Mots clés : </h2>
+          <p className={style2}>{formData.MotsCles || ""}</p>
 
-            <label htmlFor='texte' className={style1}> Texte :</label>
-            <textarea type='text' id='texte' name='texte'  rows={5} defaultValue={info.texte} className={style3} />
+          <h2 className={style1}> Texte :</h2>
+          <p className={`${style2} ml-2`}>{formData.text || ""}</p>
 
-            <label htmlFor='biblio' className={style1}> Bibiliographie : </label>
-            <textarea type='text' id='biblio' name='biblio'   rows={5} defaultValue={info.biblio} className={style3} />
-          
+          <h2 className={style1}> Bibiliographie : </h2>
+          <p className={`${style2} ml-2`}>{formData.RefBib || ""}</p>
         </div>
       )}
     </div>
--- client/frontend/src/pages/DetailsArticle.js
+++ client/frontend/src/pages/DetailsArticle.js
@@ -1,65 +1,118 @@
-import React, { useState } from 'react';
-import vector_bg from '../assets/Vector.svg';
-import InfoArticle from '../components/InfoArticle';
-
-
-// ADD HANDLE SUBLIT FOR THE BUTTON SAVE (INTEGRAION FCTN)
-// ADD INTEGRATION FOR SUPPRIMER BUTTON 
-// PiCK THE ROLE
-
-
+import React, { useState, useEffect } from "react";
+import vector_bg from "../assets/Vector.svg";
+import InfoArticle from "../components/InfoArticle";
+import axios from "axios";
 
 const DetailsArticle = () => {
-    const backgroundImage = `url(${vector_bg})`;
-    const [edit, setEdit] = useState(false);
-    const [role, setRole] = useState('modérateur');
+  const backgroundImage = `url(${vector_bg})`;
+  const [edit, setEdit] = useState(false);
+  const [role, setRole] = useState("modérateur");
+  const [articleData, setArticleData] = useState(null);
+  const [formData, setFormData] = useState(null);
 
-    return (
-        <div
-            style={{ backgroundImage }}
-            className='w-screen h-full min-h-screen  bg-purple-3OO bg-center bg-no-repeat flex flex-col items-center justify-center px-2 bg-cover pb-8 overflow-y-scroll'
-        >
+  useEffect(() => {
+    // Fetch article data from the API
+    const fetchArticleData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/api/articles/BFxWoYwBWAlgPuQlUZBw"
+        ); // Replace with your API endpoint
+        const data = await response.json();
+        setArticleData(data);
+        setFormData(data); // Set initial form data
+      } catch (error) {
+        console.error("Error fetching article data:", error);
+      }
+    };
 
+    fetchArticleData();
+  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
-            <InfoArticle edit={edit} />
+  const handleModifier = () => {
+    setEdit(true);
+    setFormData(articleData);
+  };
 
-            {role === 'modérateur' ? (
-                <div className='flex flex-row justify-center gap-4 md:gap-12 lg:gap-16 xl:gap-24'>
-                    {!edit ? (
-                        <>
-                            <button
-                                className='bg-[#ffff] text-lg font-Futura text-navBg text-center rounded-sm px-6 md:px-12 py-2'
-                                onClick={() => setEdit(true)}
-                            >
-                                Modifier
-                            </button>
-                            <button
-                                className='bg-[#6B30E4] text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2'
-                            >
-                                Supprimer
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            <button
-                                className='bg-[#6B30E4] text-lg font-Futura text-[#fff]text-center rounded-sm px-6 md:px-12 py-2'
-                                onClick={() => setEdit(false)}
-                            >
-                                Enrigistrer
-                            </button>
-                            <button
-                                className='bg-yellow text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2'
-                                onClick={() => setEdit(false)}
-                            >
-                                Annuler
-                            </button>
-                        </>
-                    )}
-                </div>
-            ) : ''}
+  const handleEnregistrer = () => {
+    // Implement the save functionality using formData
+    console.log("Save button clicked");
+    console.log(formData); // FormData is available here for further processing
+    // Add your logic to send the formData to the server or perform any other actions
+    setEdit(false);
+  };
+
+  const handleAnnuler = () => {
+    setEdit(false);
+    // Reset form data to the original article data when cancelling
+    setFormData(articleData);
+  };
+
+  const handleDelete = async () => {
+    try {
+      console.log("Article Data:", articleData);
+
+      if (articleData && articleData.id) {
+        await axios.delete(
+          `http://localhost:8000/api/articles/${articleData.id}/`
+        );
+        console.log("Article deleted successfully");
+        // Add any additional logic after deletion if needed
+      } else {
+        console.error("Invalid article data or missing ID");
+      }
+    } catch (error) {
+      console.error("Error deleting article:", error);
+    }
+  };
+
+  return (
+    <div
+      style={{ backgroundImage }}
+      className="w-screen h-full min-h-screen bg-purple-3OO bg-center bg-no-repeat flex flex-col items-center justify-center px-2 bg-cover pb-8 overflow-y-scroll"
+    >
+      {articleData ? (
+        <InfoArticle edit={edit} article={formData} />
+      ) : (
+        <p>Loading...</p>
+      )}
+
+      {role === "modérateur" && (
+        <div className="flex flex-row justify-center gap-4 md:gap-12 lg:gap-16 xl:gap-24">
+          {!edit ? (
+            <>
+              <button
+                className="bg-[#ffff] text-lg font-Futura text-navBg text-center rounded-sm px-6 md:px-12 py-2"
+                onClick={handleModifier}
+              >
+                Modifier
+              </button>
+              <button
+                className="bg-[#6B30E4] text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2"
+                onClick={handleDelete}
+              >
+                Supprimer
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="bg-[#6B30E4] text-lg font-Futura text-[#fff]text-center rounded-sm px-6 md:px-12 py-2"
+                onClick={handleEnregistrer}
+              >
+                Enregistrer
+              </button>
+              <button
+                className="bg-yellow text-lg font-Futura text-[#fff] text-center rounded-sm px-6 md:px-12 py-2"
+                onClick={handleAnnuler}
+              >
+                Annuler
+              </button>
+            </>
+          )}
         </div>
-
-    );
+      )}
+    </div>
+  );
 };
 
 export default DetailsArticle;

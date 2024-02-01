b51a8aed1bbecc99f96bfdda5db308c076c05e62
fadia <fadiabdf5@gamil.com>
2024-02-01 23:00:11 +0100

    fix the background color

--- client/frontend/src/pages/ListModerateurs.js
+++ client/frontend/src/pages/ListModerateurs.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function ListModerateurs() {
   return (
-    <div>ListModerateurs
+    <div className='bg-purple300'>ListModerateurs
       <button><Link to="/details">details</Link> </button>
     </div>
   )

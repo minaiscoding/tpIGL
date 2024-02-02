df9ec462fd92f8d815b37bf6ca389be45efcd6fa
fadia <fadiabdf5@gamil.com>
2024-02-02 19:59:12 +0100

    add components 2

---
+++ client/frontend/src/components/Moderateurs.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Moderateur from "./Moderateur";
+//
+function Moderateurs({ moderateurs, setModerateurs }) {
+  return (
+    <div className="flex flex-col  justify-start items-start h-full w-full">
+      {moderateurs.map((moderateur) => (
+        <Moderateur
+          key={moderateur.id}
+          moderateur={moderateur}
+          setModerateurs={setModerateurs}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Moderateurs;
